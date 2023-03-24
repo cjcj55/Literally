dcc0ff68730127125e27ae975e8e563faf5c21d8
Lil-Gnome-Coding <scl60@pitt.edu>
2023-03-24 19:58:47 -0400

    added the base speech recognizer code to the function

--- app/src/main/java/com/cjcj55/literallynot/ForegroundService.java
+++ app/src/main/java/com/cjcj55/literallynot/ForegroundService.java
@@ -6,10 +6,16 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 
 import androidx.core.app.NotificationCompat;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class ForegroundService extends Service {
 
@@ -33,8 +39,62 @@ public class ForegroundService extends Service {
             startForeground(1001, notification.build());
         }
         // Do your work here, such as recording audio
-        SpeechRecognizer speechRecognizer;
-        
+        SpeechRecognizer speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        final Intent spIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        spIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        spIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        spIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.getPackageName());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                //Inside of here make sure to to add the circular buffer functionality
+                ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if (matches != null && matches.size() > 0) {
+                    String spokenText = matches.get(0);
+                }
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
         // Stop the foreground service when your work is done
         //stopForeground(true);
        // stopSelf();
