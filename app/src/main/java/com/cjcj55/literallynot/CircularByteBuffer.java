870ecc2ec3e8522e50b13f1a91481cfb8af23fce
Darmiss <92101713+Darmiss@users.noreply.github.com>
2023-03-31 18:58:44 -0400

    testing circularbytebuffer and foregroundservice

---
+++ app/src/main/java/com/cjcj55/literallynot/CircularByteBuffer.java
@@ -0,0 +1,94 @@
+package com.cjcj55.literallynot;
+
+import java.util.Arrays;
+
+public class CircularByteBuffer {
+
+    private byte[] buffer;
+    private int writeIndex;
+    private int readIndex;
+    private boolean wrapped;
+
+    public CircularByteBuffer(int bufferSize) {
+        buffer = new byte[bufferSize];
+    }
+
+    public synchronized void write(byte[] data, int offset, int length) {
+        int remainingSpace = getRemainingSpace();
+        if (length > remainingSpace) {
+            int startIndex = writeIndex + remainingSpace;
+            int endIndex = startIndex + length - remainingSpace;
+            System.arraycopy(data, offset, buffer, startIndex, endIndex - startIndex);
+            System.arraycopy(data, offset + endIndex - startIndex, buffer, 0, length - endIndex + startIndex);
+            writeIndex = length - endIndex + startIndex;
+            wrapped = true;
+        } else {
+            int endIndex = writeIndex + length;
+            if (endIndex < buffer.length) {
+                System.arraycopy(data, offset, buffer, writeIndex, length);
+                writeIndex = endIndex;
+            } else {
+                int startIndex = endIndex - buffer.length;
+                System.arraycopy(data, offset, buffer, writeIndex, length - startIndex);
+                System.arraycopy(data, offset + length - startIndex, buffer, 0, startIndex);
+                writeIndex = startIndex;
+                wrapped = true;
+            }
+        }
+    }
+
+    public synchronized int read(byte[] data, int offset, int length) {
+        int availableData = getAvailableData();
+        if (length > availableData) {
+            length = availableData;
+        }
+        if (length > 0) {
+            if (readIndex + length <= buffer.length) {
+                System.arraycopy(buffer, readIndex, data, offset, length);
+                readIndex += length;
+                if (readIndex == buffer.length) {
+                    readIndex = 0;
+                    wrapped = false;
+                }
+            } else {
+                int startIndex = readIndex;
+                int endIndex = startIndex + length - (buffer.length - startIndex);
+                System.arraycopy(buffer, startIndex, data, offset, buffer.length - startIndex);
+                System.arraycopy(buffer, 0, data, offset + buffer.length - startIndex, endIndex);
+                readIndex = endIndex;
+                wrapped = false;
+            }
+        }
+        return length;
+    }
+
+    public synchronized byte[] readAll() {
+        int availableData = getAvailableData();
+        byte[] data = new byte[availableData];
+        read(data, 0, availableData);
+        return data;
+    }
+
+    public synchronized void clear() {
+        readIndex = 0;
+        writeIndex = 0;
+        wrapped = false;
+    }
+
+    public synchronized int getAvailableData() {
+        if (wrapped) {
+            return buffer.length - readIndex + writeIndex;
+        } else {
+            return writeIndex - readIndex;
+        }
+    }
+
+    public synchronized int getRemainingSpace() {
+        if (wrapped) {
+            return readIndex - writeIndex;
+        } else {
+            return buffer.length - writeIndex;
+        }
+    }
+
+}
--- app/src/main/java/com/cjcj55/literallynot/ForegroundService.java
+++ app/src/main/java/com/cjcj55/literallynot/ForegroundService.java
@@ -1,11 +1,14 @@
 package com.cjcj55.literallynot;
 
+import static android.app.Service.START_STICKY;
+
+import android.Manifest;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
@@ -14,32 +17,72 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
-import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
-import java.util.concurrent.TimeUnit;
+import android.util.Log;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 
-import java.io.ByteArrayOutputStream;
+import com.cjcj55.literallynot.R;
+
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Locale;
 
-public class ForegroundService extends Service implements Runnable{
+public class ForegroundService extends Service {
+    private static final String TAG = "ForegroundService";
+    private static final int SAMPLE_RATE = 44100;
+    private static final int BUFFER_SIZE = SAMPLE_RATE * 2; // 2 seconds of audio buffer
+    private static final int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
+    private static final int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;
+    private static final String KEYWORD = "literally";
+    private static final int KEYWORD_CONTEXT_TIME = 5000; // 5 seconds before and after the keyword
+    private static final String CHANNEL_ID = "test";
 
-    private static final int NOTIFICATION_ID = 1;
+    private AudioRecord mAudioRecord;
+    private CircularByteBuffer mAudioBuffer;
+    private SpeechRecognizer mSpeechRecognizer;
+    private HandlerThread mHandlerThread;
+    private Handler mHandler;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+
+
+
+
+
+        mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE);
+        mAudioBuffer = new CircularByteBuffer(BUFFER_SIZE);
+        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mHandlerThread = new HandlerThread(TAG);
+        mHandlerThread.start();
+        mHandler = new Handler(mHandlerThread.getLooper());
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // Create a notification for the foreground service
-        final String CHANNEL_ID = "Foreground Service";
-        NotificationChannel channel = null;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            channel = new NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_LOW);
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_LOW);
 
             getSystemService(NotificationManager.class).createNotificationChannel(channel);
             Notification.Builder notification = new Notification.Builder(this, CHANNEL_ID)
@@ -50,157 +93,142 @@ public class ForegroundService extends Service implements Runnable{
             // Start the foreground service with the notification
             startForeground(1001, notification.build());
         }
-        // Do your work here, such as recording audio
-        SpeechRecognizer speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-        final Intent spIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        spIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        spIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        spIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.getPackageName());
-
-        //Initializes the MediaRecorder for audio recording
-        PackageManager m = getPackageManager();
-        String s = getPackageName();
-        PackageInfo p = null;
-        try {
-            p = m.getPackageInfo(s, 0);
-        } catch (PackageManager.NameNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        s = p.applicationInfo.dataDir;
-        System.out.println(s);
-        System.out.println(m);
-        System.out.println(p);
-
-        AudioRecorder recorder = new AudioRecorder();
-        try {
-            recorder.startRecording();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-//        recorder.stopRecording();
-        //speechRecognizer.startListening(spIntent);
-        speechRecognizer.setRecognitionListener(new RecognitionListener() {
-            @Override
-            public void onReadyForSpeech(Bundle bundle) {
-
-            }
-
-            @Override
-            public void onBeginningOfSpeech() {
-
-            }
-
-            @Override
-            public void onRmsChanged(float v) {
-
-            }
-
-            @Override
-            public void onBufferReceived(byte[] bytes) {
-
-            }
-
-            @Override
-            public void onEndOfSpeech() {
-
-            }
-
-            @Override
-            public void onError(int i) {
-
-            }
-
-            @Override
-            public void onResults(Bundle results) {
-                //Inside of here make sure to to add the circular buffer functionality
-                ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                if (matches != null && matches.size() > 0) {
-                    String spokenText = matches.get(0);
-                    System.out.println(spokenText);
-                }
-            }
-
-            @Override
-            public void onPartialResults(Bundle bundle) {
-
-            }
-
-            @Override
-            public void onEvent(int i, Bundle bundle) {
-
-            }
-        });
-        // Stop the foreground service when your work is done
-        //stopForeground(true);
-        // stopSelf();
-
-        return START_NOT_STICKY;
+        mAudioRecord.startRecording();
+        mHandler.post(new AudioReader());
+        return START_STICKY;
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mHandlerThread.quitSafely();
+        mAudioRecord.stop();
+        mAudioRecord.release();
+        mSpeechRecognizer.destroy();
+    }
+
+    private class AudioReader implements Runnable {
+        private final Handler mHandler = new Handler();
+
+        @Override
+        public void run() {
+            System.out.println("AudioBuffer run()");
+            byte[] buffer = new byte[BUFFER_SIZE];
+            while (true) {
+                int bytesRead = mAudioRecord.read(buffer, 0, buffer.length);
+                mAudioBuffer.write(buffer, 0, bytesRead);
+                mHandler.post(new RecognizeSpeechTask(mAudioBuffer.readAll()));
+                System.out.println("Everything in audio buffer " + mAudioBuffer.readAll());
+            }
+        }
+    }
+
+    private class RecognizeSpeechTask implements Runnable {
+        private final byte[] mAudioData;
+
+        RecognizeSpeechTask(byte[] audioData) {
+            mAudioData = audioData;
+        }
+
+        @Override
+        public void run() {
+            String text = recognizeSpeech(mAudioData);
+            System.out.println("Run called, checking for keyword");
+            if (text.contains(KEYWORD)) {
+                long keywordTimestamp = getTimestampForKeyword(text, KEYWORD);
+                byte[] keywordAudioData = getAudioDataForTimestamp(keywordTimestamp);
+                saveAudioToFile(keywordAudioData, getOutputFilePath());
+            }
+        }
+    }
+    private String recognizeSpeech(byte[] audioData) {
+        System.out.println("recognizeSpeech called");
+        String text = "";
+        if (audioData.length >= 0) {
+            System.out.println("> 0?");
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+            intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, 2000);
+            intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, 3000);
+            intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, 3000);
+            try {
+                mSpeechRecognizer.startListening(intent);
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to recognize speech", e);
+            }
+        }
+        return text;
+    }
+
+    private long getTimestampForKeyword(String text, String keyword) {
+        int startIndex = text.indexOf(keyword);
+        long keywordStartTime = getAudioTimestampForTextIndex(text, startIndex);
+        long keywordEndTime = keywordStartTime + KEYWORD_CONTEXT_TIME;
+        return keywordEndTime;
+    }
+
+    private long getAudioTimestampForTextIndex(String text, int index) {
+        int numSamples = index * SAMPLE_RATE / 1000;
+        return (long) numSamples * 1000 / SAMPLE_RATE;
+    }
+
+    private byte[] getAudioDataForTimestamp(long timestamp) {
+        byte[] audioData = mAudioBuffer.readAll();
+        int startIndex = getIndexForAudioTimestamp(audioData.length, timestamp - KEYWORD_CONTEXT_TIME);
+        int endIndex = getIndexForAudioTimestamp(audioData.length, timestamp + KEYWORD_CONTEXT_TIME);
+        return Arrays.copyOfRange(audioData, startIndex, endIndex);
+    }
+
+    private int getIndexForAudioTimestamp(int audioLength, long timestamp) {
+        System.out.println("getIndexForAudioTimestamp");
+        int numSamples = (int) (timestamp * SAMPLE_RATE / 1000);
+        int numBytes = numSamples * 2;
+        int index = audioLength - numBytes;
+        if (index < 0) {
+            index = 0;
+        }
+        return index;
+    }
+
+    private void saveAudioToFile(byte[] audioData, String filePath) {
+        try {
+            System.out.println("saving audio");
+            FileOutputStream fos = new FileOutputStream(filePath);
+            fos.write(audioData);
+            fos.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to save audio file", e);
+        }
+    }
+
+    private String getOutputFilePath() {
+        System.out.println("getting outputfile path");
+        File dir = getCacheDir();
+        return new File(dir, "recorded_audio_" + System.currentTimeMillis() + ".mp3").getAbsolutePath();
+    }
+
+    private Notification buildNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel_id")
+                .setContentTitle("Foreground Service")
+                .setContentText("Recording audio...")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_SERVICE)
+                .setOngoing(true);
+        Intent stopIntent = new Intent(this, ForegroundService.class);
+        stopIntent.setAction("stop");
+        PendingIntent pendingIntent = PendingIntent.getService(this, 0, stopIntent, 0);
+        builder.addAction(R.drawable.ic_launcher_background, "Stop", pendingIntent);
+        return builder.build();
+    }
+
+    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        // This service does not support binding, so return null
         return null;
     }
-
-    @Override
-    public void run() {
-
-    }
-    //In order to get context for file location was required to
-    public class AudioRecorder{
-
-        private MediaRecorder recorder;
-        private ByteArrayOutputStream output;
-        private Handler handler;
-        private Runnable callback;
-        //Constructor  for the Audio Recorder.  Thank you sleep deprivation
-        public AudioRecorder() {
-            //Initialize the AudioRecorder variables
-            recorder = new MediaRecorder();
-            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-            recorder.setAudioSamplingRate(44100);
-            recorder.setAudioChannels(AudioFormat.ENCODING_DEFAULT);  //Doesn't crash program when set to DEFAUKT
-            recorder.setAudioEncodingBitRate(AudioFormat.ENCODING_MP3);
-            //Create the output stream for the thing
-            output = new ByteArrayOutputStream();
-            handler = new Handler();
-
-        }
-
-        public void startRecording() throws IOException{
-            //outputFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/AudioRecording_" + System.currentTimeMillis() + ".mp3");
-            //If we can get setOutputFile Working this will be completed
-            String audioFileName = "myaudiofile.mp3";
-            File outputFile = new File(getCacheDir(), audioFileName);
-            recorder.setOutputFile(outputFile.getAbsolutePath());
-            recorder.prepare();
-            recorder.start();
-            callback = new Runnable() {
-                @Override
-                public void run() {
-                    byte[] recordBytes = output.toByteArray();
-                    String recordedText = new String(recordBytes, StandardCharsets.UTF_8);
-                    System.out.println(recordedText);
-                    handler.postDelayed(this, 50000);
-                }
-            };
-            handler.postDelayed(callback, 50000);
-
-        }
-        public void stopRecording(){
-            if(null != recorder){
-                recorder.stop();
-                recorder.release();
-                recorder = null;
-                handler.removeCallbacks(callback);
-
-            }
-        }
-        public void recordLoop(){
-
-        }
-    }
 }
+
+
