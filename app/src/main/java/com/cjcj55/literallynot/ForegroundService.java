e289ab5b0287fd9651b689648990537aabfac551
Darmiss <92101713+Darmiss@users.noreply.github.com>
2023-04-21 17:52:41 -0400

    optimization and vibration
    foreground service should be much faster now...

--- app/src/main/java/com/cjcj55/literallynot/ForegroundService.java
+++ app/src/main/java/com/cjcj55/literallynot/ForegroundService.java
@@ -128,7 +128,8 @@ public class ForegroundService extends Service {
             startForeground(1001, notification.build());
         }
         mAudioRecord.startRecording();
-        mHandler.post(new AudioReader());
+        // Run the AudioReader on a background thread
+        new AudioReader().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         return START_STICKY;
     }
 
@@ -149,19 +150,19 @@ public class ForegroundService extends Service {
     }
 
 
-    private class AudioReader implements Runnable {
-        private final Handler mHandler = new Handler();
-
+    private class AudioReader extends AsyncTask<Void, Void, Void> {
         @Override
-        public void run() {
+        protected Void doInBackground(Void... voids) {
             System.out.println("AudioBuffer run()");
             byte[] buffer = new byte[BUFFER_SIZE];
             while (mAudioRecord.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING) { //check if mAudioRecord is still recording before attempting to read from it,
                 System.out.println("READING...WRITING AUDIO TO BUFFER.");
                 int bytesRead = mAudioRecord.read(buffer, 0, buffer.length);
+                System.out.println("BYTEREAD" + bytesRead);
                 mAudioBuffer.write(buffer, 0, bytesRead);
-               mHandler.post(new RecognizeSpeechTask(mAudioBuffer.readAll()));
+                mHandler.post(new RecognizeSpeechTask(mAudioBuffer.readAll()));
             }
+            return null;
         }
     }
 
@@ -170,6 +171,7 @@ public class ForegroundService extends Service {
 
         RecognizeSpeechTask(byte[] audioData) {
             mAudioData = audioData;
+            System.out.println("audiodata given length" + audioData.length);
         }
 
         @Override
@@ -203,15 +205,16 @@ public class ForegroundService extends Service {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
                 }
+
                 // Play notification sound
                 Uri notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
                 Ringtone ringtone = RingtoneManager.getRingtone(getApplicationContext(), notificationSound);
                 ringtone.play();
                 //UNCOMMENT THIS TO SAVE YOUR LITERALLY SNIPPED TO THE DB
-               // saveAudioToFile(mAudioData, getOutputFilePath());
+                // saveAudioToFile(mAudioData, getOutputFilePath());
                 File file = new File(getCacheDir(), "recordedAudio.mp3");
                 //LIKEWISE
-               // MySQLHelper.writeAudioFile(getApplicationContext(), file, text);
+                // MySQLHelper.writeAudioFile(getApplicationContext(), file, text);
             }
         }
 
@@ -270,10 +273,11 @@ public class ForegroundService extends Service {
             Log.e(TAG, "Error saving audio to file: " + e.getMessage());
         }
     }
+
     private String getOutputFilePath() {
         System.out.println("getting outputfile path");
         File dir = getCacheDir();
-        return new File(dir,"recordedAudio.mp3").getAbsolutePath();
+        return new File(dir, "recordedAudio.mp3").getAbsolutePath();
     }
 
 
@@ -282,7 +286,6 @@ public class ForegroundService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
-
 
 
     private class GeocodeTask extends AsyncTask<Void, Void, List<Address>> {
@@ -297,6 +300,7 @@ public class ForegroundService extends Service {
         @Override
         protected List<Address> doInBackground(Void... voids) {
             Geocoder geocoder = new Geocoder(ForegroundService.this, Locale.getDefault());
+            System.out.println("HITGEOCODE");
             List<Address> addresses = null;
             try {
                 addresses = geocoder.getFromLocation(latitude, longitude, 1);
@@ -330,9 +334,6 @@ public class ForegroundService extends Service {
     }
 
 
-
-
-
     private class LocationTask extends AsyncTask<Void, Void, Void> {
 
         private FusedLocationProviderClient fusedLocationProviderClient;
@@ -359,9 +360,7 @@ public class ForegroundService extends Service {
                         double longitude = location.getLongitude();
                         Log.d("TAG", "Latitude: " + latitude + ", Longitude: " + longitude);
 
-                        GeocodeTask geocodeTask = new GeocodeTask(latitude, longitude); //Running geocode on asyntask in background
-                        geocodeTask.execute();
-                        //Address is in here ^
+                        getAddressFromLocation(latitude, longitude);
                     }
                 }
             };
@@ -379,8 +378,33 @@ public class ForegroundService extends Service {
             // Perform background tasks (if any)
             return null;
         }
+
+        private void getAddressFromLocation(double latitude, double longitude) {
+            Geocoder geocoder = new Geocoder(ForegroundService.this, Locale.getDefault());
+            System.out.println("HITGEOCODE");
+            List<Address> addresses = null;
+            try {
+                addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (addresses != null && addresses.size() > 0) {
+                Address address = addresses.get(0);
+                // extract the address components and display them
+                String addressLine = address.getAddressLine(0);
+                String city = address.getLocality();
+                String state = address.getAdminArea();
+                String country = address.getCountryName();
+                String postalCode = address.getPostalCode();
+                String knownName = address.getFeatureName();
+                // do something with the address information here
+                Log.d("TAG", address.toString());
+                Toast.makeText(ForegroundService.this, address.toString(), Toast.LENGTH_LONG).show();
+
+                //TODO: SEND TO DATABASE
+            }
+        }
     }
-
-
 }
 
